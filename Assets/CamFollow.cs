651b266a02331f5753fae7175cb08c4289e2773e
Treton Baptiste <vergildu49@gmail.com>
2020-03-18 16:20:45 +0100

    Creating isometric camera

---
+++ Assets/CamFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+
+    Transform targetToFollow;
+
+    void Start()
+    {
+        targetToFollow = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(targetToFollow.position.x - 10f, 10f, targetToFollow.position.z - 10f);
+    }
+}
---
+++ Assets/CharacterContol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterContol : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 4f;
+
+    Vector3 forward, right;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+            Move();
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
+        Vector3 rightMove = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMove = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMove + upMove);
+
+        transform.forward = heading;
+        transform.position += rightMove;
+        transform.position += upMove;
+    }
+}
