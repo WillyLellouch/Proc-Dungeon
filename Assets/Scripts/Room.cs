bc7aae2f274e42e33c7b44e68a5783eab22922a1
Nelyanen <dreamer.chrysalis@gmail.com>
2020-03-18 16:57:36 +0100

    Rework of filesystem

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoomType
+{
+    EMPTY_ROOM,
+    TREASURE_ROOM,
+    CORRIDOR,
+    BOSS_ROOM,
+    ENTRANCE_ROOM
+}
+
+public class Room
+{
+    public Vector2 gridPos;
+
+    public RoomType type;
+
+    public Room north, east, south, west;
+
+    public Room(Vector2 _gridPos, RoomType _type)
+    {
+        gridPos = _gridPos;
+        type = _type;
+    }
+}
